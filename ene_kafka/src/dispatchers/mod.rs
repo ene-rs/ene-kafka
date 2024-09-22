bca90b530a0b80ec0a31d52985058a27d4325bf4
Abdullahsab3 <abdallah-sab3@hotmail.com>
2024-09-22 13:41:50 +0200

    remove dead code

--- ene_kafka/src/dispatchers/mod.rs
+++ ene_kafka/src/dispatchers/mod.rs
@@ -10,30 +10,12 @@ pub trait EventDispatcher: Send + Sync {
     ) -> anyhow::Result<()>;
 }
 
-#[macro_export]
-macro_rules! generate_handler_types_enum {
-    ($($handler: ident $(< $( $identifier:tt $( : $identifier_constraint:tt $(+ $identifier_additions:tt )* )? ),+ >)?),*) => {
-        enum HandlerTypes {
-            $($handler($handler $(< $( $generic_identifier $( : $identifier_constraint $(+ $identifier_additions )* )? ),+ >)?)),*
-        }
-
-        $(
-            impl From<$handler $(< $( $generic_identifier $( : $identifier_constraint $(+ $identifier_additions )* )? ),+ >)?> for HandlerTypes {
-                fn from(handler: $handler $(< $( $generic_identifier $( : $identifier_constraint $(+ $identifier_additions )* )? ),+ >)?) -> Self {
-                    HandlerTypes::$handler(handler)
-                }
-            }
-        )*
-    }
-}
-
 /// A macro to generate an event dispatcher struct that will dispatch events to the appropriate handlers
 /// based on the event type.
 /// The macro expects a list of handlers that will be used to dispatch the events.
 #[macro_export]
 macro_rules! generate_event_dispatcher {
     ($($handler_name: ident: $handler_type: ident $(< $( $generic_identifier:tt $( : $identifier_constraint:tt $(+ $identifier_additions:tt )* )? ),+ >)?),*) => {
-       // ene_kafka::generate_handler_types_enum!($($handler $(< $( $generic_identifier $( : $identifier_constraint $(+ $identifier_additions )* )? ),+ >)?),*);
         struct CloudEventDispatcher {
            $(
                $handler_name: $handler_type $(< $( $generic_identifier $( : $identifier_constraint $(+ $identifier_additions )* )? ),+ >)?,
